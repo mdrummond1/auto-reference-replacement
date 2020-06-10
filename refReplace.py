414062c076f9bae98b4b4c53a35c264b69832f79
Matthew Drummond <mattatatter@gmail.com>
2020-06-10 08:06:35 -0500

    added ability to find reference from reading txt file

--- refReplace.py
+++ refReplace.py
@@ -1,33 +1,36 @@
 import xml.etree.ElementTree as ET
+from itertools import cycle
 
 esv = ET.parse('bibles/ESV')
+""" kjv = ET.parse('bibles/KJV.xmm')
+nasb = ET.parse('bibles/NASB.xmm')
+nkjv = ET.parse('bibles/NKJV.xmm') """
+
 bible = esv.getroot()
 notes = open("test.txt")
 count = 0
 notes = notes.read().split()
-print(notes)
 
-ref = []
+#print(notes)
+
 
 for word in notes:
     if word[0] == '~':
-        ref.append(word)
-
-print(ref)
-
-'''for book in bible:
-    
-    for chapter in book:
-                #sql.write("\n\t\tChapter " + chapter.attrib['n'])
-  #              if chapter.attrib['n'] == userChapter:
+        if (notes.index(word) + 1 != len(notes)):
+            book = word[1:]
+            chapter = int(notes[notes.index(word)+1].split(':')[0]) - 1
+            print(notes[notes.index(word)+1].split(':'))
+            verse = int(notes[notes.index(word)+1].split(':')[1]) - 1
+            print("{} {}:{} - ".format(book, chapter + 1, verse + 1))
+            for bookNDX in bible:
                 
-        for verse in chapter:
-                        #sql.write("\t\t\t" + str(verse.attrib['n']) + " " + str(verse.text) + "\n")
-                        #print("\t\t\t" + str(verse.attrib['n']) + " " + str(verse.text) + "\n")
- #                       if verse.attrib['n'] == userVerse:
-            print(book.attrib['n'] + " " + chapter.attrib['n'] + ":" + verse.attrib['n'] + "-" + verse.text)
-            count = count + 1
-            print("There are " + str(count) + " verses in the Bible")
-#    userChoice = input("Search Again? (y/n)\n")
-  '''                      
+                if bookNDX.attrib['n'].lower() == book.lower():
+                    print(bookNDX[chapter][verse].text)
+
+
+
+
+       
+
+                      
 
